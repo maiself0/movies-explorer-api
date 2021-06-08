c196c9c7556d56f17f7ce823fbf1eae3f1a7007b
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2021-06-08 22:16:45 +0300

    fix after review part 1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/auth-err');
 
--- utils/celebrateConfig.js
+++ utils/celebrateConfig.js
@@ -5,7 +5,7 @@ module.exports.celebrateSignup = celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
   }),
 });
 
