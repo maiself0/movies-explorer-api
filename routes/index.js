1016f453534f80f66dcbe697cb99f393633e3cd6
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2021-07-11 18:27:22 +0300

    fix: router(celebrateauth)

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/not-found-err');
-const { celebrateSignup, celebrateSingin, celebrateAuth } = require('../utils/celebrateConfig');
+const { celebrateSignup, celebrateSingin } = require('../utils/celebrateConfig');
 
 router.post('/signup', celebrateSignup, createUser);
 router.post('/signin', celebrateSingin, login);
-router.use('/users', celebrateAuth, auth, require('./users'));
-router.use('/movies', celebrateAuth, auth, require('./movies'));
+router.use('/users', auth, require('./users'));
+router.use('/movies', auth, require('./movies'));
 
 router.get('/', auth);
 
